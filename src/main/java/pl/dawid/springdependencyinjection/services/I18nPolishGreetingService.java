f8109abae9fb125881bc564dc29e90233808e905
Vartonix <szafkadawid001@gmail.com>
2022-06-29 22:42:08 +0200

    Default Profiles

--- src/main/java/pl/dawid/springdependencyinjection/services/I18nPolishGreetingService.java
+++ src/main/java/pl/dawid/springdependencyinjection/services/I18nPolishGreetingService.java
@@ -3,7 +3,7 @@ package pl.dawid.springdependencyinjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("PL")
+@Profile({"PL", "default"})
 @Service("i18nService")
 public class I18nPolishGreetingService implements GreetingService{
     @Override
